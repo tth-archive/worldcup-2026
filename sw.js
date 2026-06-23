0464a05d2f77a39f52a1a6c42ca6c12327b85870
tth-archive <michellett0105@gmail.com>
2026-06-23 20:39:58 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,14 @@
-const C="wc2026-v9";
+const C="wc2026-v10";
 const A=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting()));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener("fetch",e=>{
+  const req=e.request;
+  if(req.method!=="GET")return;
+  e.respondWith(
+    fetch(req).then(res=>{
+      if(res&&res.ok&&new URL(req.url).origin===location.origin){const cp=res.clone();caches.open(C).then(c=>c.put(req,cp));}
+      return res;
+    }).catch(()=>caches.match(req).then(r=>r||caches.match("./index.html")))
+  );
+});
